37da22cb73a83145e69187e807f60281fcd4510e
Peace <PeaceUCR@users.noreply.github.com>
2018-01-22 14:29:58 -0800

    bind this in the constructor
    bind this in the constructor

--- src/Clock.js
+++ src/Clock.js
@@ -6,16 +6,14 @@ class Clock extends Component{
     constructor(props){
         super(props);
         this.state={date:new Date()};
+        this.update= this.update.bind(this);
     }
     componentDidMount(){
-        var update = function () {
-
-            this.setState({date: new Date()});
-
-        }.bind(this);
         setInterval(update,1000);// we can't directly call update here, otherwise will lost this
     }
-
+    update(){
+        this.setState({date: new Date()});
+    }
     render(){
         return (<p>{this.state.date.toLocaleTimeString()}</p>);
     }
